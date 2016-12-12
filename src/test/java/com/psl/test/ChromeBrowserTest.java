8c98905930f4c83d0c056caf9cb82f3730aa8267
Shubham Yannawar <shubham_yannawar@persistent.co.in>
2016-12-12 17:34:23 +0530

    Browser Tests 4

--- src/test/java/com/psl/test/ChromeBrowserTest.java
+++ src/test/java/com/psl/test/ChromeBrowserTest.java
@@ -1,11 +1,12 @@
 package com.psl.test;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeBrowserTest {
@@ -20,8 +21,8 @@ public class ChromeBrowserTest {
 	public void chromeTest(){
 		chrome.get("http://localhost:8080/BankApp/");
 		WebElement userName = chrome.findElement(By.id("test1"));
-		String s = userName.getTagName();
-		Assert.assertTrue(s.equals("button"));
+		 String s = userName.getTagName();
+		 Assert.assertTrue(s.equals("button"));
 	}
 	@After
 	public void tearDown(){
